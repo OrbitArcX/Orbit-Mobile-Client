f82bf2061267420b31549965306b202ccc635d5e
IT21036620 <it21036620@my.sliit.lk>
2024-10-03 02:45:04 +0530

    retrofit configured for api calls

--- app/src/main/java/com/example/orbitmobile/LoginActivity.java
+++ app/src/main/java/com/example/orbitmobile/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.orbitmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,16 +20,22 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        TextView createAccountLink = findViewById(R.id.create_account_link);
-        Button getStartedButton = findViewById(R.id.continue_button);
+        TextView createAccountLink = findViewById(R.id.create_one);
+        Button loginButton = findViewById(R.id.continue_button);
 
 
         createAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle account creation or navigate to account creation activity
-                // For now, just showing a message
-                // TODO: Add logic to handle account creation
+                Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
             }
         });
 
