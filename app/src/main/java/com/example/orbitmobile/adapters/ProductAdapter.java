083c9a7e03f782a27c1efc24cdbcb0ed6704bd4b
IT21036620 <it21036620@my.sliit.lk>
2024-10-03 18:10:01 +0530

    Adapters for recycleview created

---
+++ app/src/main/java/com/example/orbitmobile/adapters/ProductAdapter.java
@@ -0,0 +1,58 @@
+package com.example.orbitmobile.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.orbitmobile.R;
+import com.example.orbitmobile.models.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private List<Product> productList;
+    private Context context;
+
+    public ProductAdapter(List<Product> productList, Context context) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.productName.setText(product.getName());
+        holder.productPrice.setText("$" + product.getPrice());
+        Glide.with(context).load(product.getImageUrl()).into(holder.productImage);  // Use Glide to load images
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+        TextView productName, productPrice;
+        ImageView productImage;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productName = itemView.findViewById(R.id.product_name);
+            productPrice = itemView.findViewById(R.id.product_price);
+            productImage = itemView.findViewById(R.id.product_image);
+        }
+    }
+}
